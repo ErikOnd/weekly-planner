bce8df6694a517709f6062ff271139b3325e04e8
Erik <erik.ondra@yahoo.com>
2025-11-09 10:43:12 +0100

    redirection for login page when already logged in

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,8 +1,18 @@
 import styles from "./LoginPage.module.scss";
 
 import { AuthForm } from "@components/auth/AuthForm";
+import { createClient } from "@utils/supabase/server";
+import { redirect } from "next/navigation";
 
-export default function LoginPage() {
+export default async function LoginPage() {
+	const supabase = await createClient();
+
+	const { data, error } = await supabase.auth.getUser();
+
+	if (!error && data?.user) {
+		redirect("/");
+	}
+
 	return (
 		<main className={styles["login-holder"]}>
 			<AuthForm />
